f46dc34d4d84703b995ca7dd5fdde9fe3cddfcff
johnburitto <79087305+johnburitto@users.noreply.github.com>
2024-07-02 12:21:49 +0300

    Eco functionality

---
+++ SoulfulConversationsBot/Commands/RepeatCommand.cs
@@ -0,0 +1,14 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace SoulfulConversationsBot.Commands
+{
+    public class RepeatCommand : BaseCommandModule
+    {
+        [Command("repeat")]
+        public async Task ExecuteAsync(CommandContext ctx, params string[] data)
+        {
+            await ctx.Channel.SendMessageAsync(string.Join(" ", data));
+        }
+    }
+}
--- SoulfulConversationsBot/Program.cs
+++ SoulfulConversationsBot/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// MTI1NzYwODE3NTU3OTAzNzc2Ng.GBrGmt.4sON2rmFpOE7VOdEGJ41YvTF2IoN-OqsVc4KVE
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using SoulfulConversationsBot.Commands;
+
+// TODO: Окремий клас для конфігруації бота та команд. Зробити конфігуратор
+var configuration = new DiscordConfiguration()
+{
+    Intents = DiscordIntents.All, 
+    Token = "MTI1NzYwODE3NTU3OTAzNzc2Ng.GBrGmt.4sON2rmFpOE7VOdEGJ41YvTF2IoN-OqsVc4KVE",
+    TokenType = TokenType.Bot,
+    AutoReconnect = true
+};
+var cammandsNextConfiguration = new CommandsNextConfiguration()
+{
+    StringPrefixes = ["!"],
+    EnableMentionPrefix = true,
+    EnableDms = true,
+    EnableDefaultHelp = false
+};
+var client = new DiscordClient(configuration);
+var commands = client.UseCommandsNext(cammandsNextConfiguration);
+
+client.Ready += ClientReadyAsync;
+commands.RegisterCommands<RepeatCommand>();
+
+await client.ConnectAsync();
+await Task.Delay(-1);
+
+static Task ClientReadyAsync(DiscordClient sender, ReadyEventArgs args)
+{
+    return Task.CompletedTask;
+}
