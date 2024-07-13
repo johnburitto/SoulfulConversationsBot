401c801397743dcb3737b3ad5d947c096e13bf8e
Dead0utside <volodya1234321@gmail.com>
2024-07-13 19:24:06 +0100

    new command: coinflip

---
+++ SoulfulConversationsBot/Commands/CoinFlip.cs
@@ -0,0 +1,24 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace SoulfulConversationsBot.Commands
+{
+    public class CoinFlip : BaseCommandModule
+    {
+        private readonly Random _rng = new Random();
+        [Command("coinflip")]
+        public async Task ExecuteAsync(CommandContext context)
+        {
+            string result;
+            if (_rng.Next(2) == 0)
+            {
+                result = "Орел";
+            }
+            else
+            {
+                result = "Решка";
+            }
+            await context.Channel.SendMessageAsync("Результат: " + result);
+        }
+    }
+}
