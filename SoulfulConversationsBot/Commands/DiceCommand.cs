78d7be692229f3c6ba9e23fb94508093572a4d5f
Dead0utside <volodya1234321@gmail.com>
2024-07-19 17:03:52 +0100

    implemented !dice command

---
+++ SoulfulConversationsBot/Commands/DiceCommand.cs
@@ -0,0 +1,21 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using SoulfulConversationsBot.Configuration;
+
+namespace SoulfulConversationsBot.Commands
+{
+    public class DiceCommand : BaseCommandModule
+    {
+        private readonly Random _rng = new Random();
+
+        [Command("dice")]
+        public async Task ExecuteAsync(CommandContext context)
+        {
+            var result = string.Format("DiceFaces:Dice{0}", _rng.Next(1, 7));
+            var embedMessage = new DiscordEmbedBuilder().WithImageUrl(ConfigurationManager.GetValue<string>(result));
+
+            await context.Channel.SendMessageAsync(embed: embedMessage);
+        }
+    }
+}
